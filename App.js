42ef64cb6c28c43282cbfb061c0a9feb73914651
marcos-luz <marcos.antonioluz36@gmail.com>
2020-12-16 19:41:10 -0300

    Initializing the project

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{fontSize: 46, color: '#fff', textAlign: 'center'}}>Lucas Barbosa Leite Condensado</Text>
+      <Image source={{uri: 'https://api.tendaatacado.com.br/fotos/1653_big.jpg'}} style={{width: 120, height: 120, marginVertical: 15}} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +15,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
