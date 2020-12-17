eeee28b65305d1a983b0382d1ca30787827de8aa
Racklyn <racklynsotero@gmail.com>
2020-12-16 23:23:31 -0300

    Listagem de Itens

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image } from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler'
 //import { Octicons, MaterialIcons } from '@expo/vector-icons';
 
-const Item = (name) => {
+const Item = ({name}) => {
   return(
     <TouchableOpacity style={styles.container}>
-        {name}
+        <Text style={styles.text}>{name}</Text>
     </TouchableOpacity>
   );
 }
@@ -14,7 +15,12 @@ export default Item;
 
 const styles = StyleSheet.create({
   container: {
-    width: '90%',
-    backgroundColor: '#F00'
+    width: 100,
+    backgroundColor: '#82B',
+    height: 50,
+    margin: 10
+  },
+  text:{
+    color: '#FFF'
   }
 });
--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,34 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react';
 import { StyleSheet, TextInput, Text, View, Image, Dimensions} from 'react-native';
-import api from '../utils';
-import Title from '../src/components/Title';
+import {ScrollView } from 'react-native-gesture-handler';
+import api from '../../utils/api';
+import Item from '../components/Item';
+import Title from '../components/Title';
 
 const width = Dimensions.get('screen').width;
 
 export default function HomeScreen() {
 
-  const [country, setCountry] = useState('...');
+  //const [country, setCountry] = useState('...');
+  const [countries, setCountries] = useState([
+    {'name':"A"},
+    {'name':"B"},
+    {'name':"C"},
+  ])
 
   useEffect(() => {
-    api.get(`name/brasil`).then((resp)=>{
-      //alert(resp.data[0].region);
-      //setCountry(resp.data)
-    });
+    // api.get(`name/brasil`).then((resp)=>{
+    //   //alert(resp.data[0].region);
+    //   //setCountry(resp.data)
+    // });
   },[])
 
-  function setRegion(text) {
+  function setCountry(text) {
     api.get(`name/${text}`).then((resp) => {
-      if(text.length>=3){
-        setCountry(resp.data[0].region);  
+      if(text.length>=1){
+        setCountries(resp.data)
+        
+        //setCountry(resp.data[0].region);  
       } 
       
       else {
-        setCountry('?');
+        setCountries([])
       }
 
     }).catch((e) => {
-      setCountry('?');
+      setCountries([])
     })
       
   }
@@ -39,9 +48,21 @@ export default function HomeScreen() {
       <Title/>
       <TextInput
         style={styles.input}
-        onChangeText={text => setRegion(text)}
+        onChangeText={text => setCountry(text)}
       />
-      <Text>Região: {country}.</Text>
+      
+      {/* <FlatList
+        style={styles.list}
+        data={countries}
+        keyExtractor={c=>c.name}
+        renderItem={(item)=>(
+          <Item name={item.name}/>
+        )}
+      /> */}
+      <ScrollView style={styles.list}>
+        {countries.map(e=><Item name={e.name}/>)}
+      </ScrollView>
+
     </View>
   );
 }
@@ -60,5 +81,15 @@ const styles = StyleSheet.create({
     borderColor: 'gray', 
     borderWidth: 1,
     paddingHorizontal: 15 
+  },
+  list:{
+    marginTop:15,
+    width: '95%',
+    borderColor: '#58F',
+    borderWidth: 2,
+    borderRadius: 10,
+    borderBottomWidth: 0,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 0,
   }
 });
