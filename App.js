e078f2a948bd5baba3fca9c374e41a787cd16e1b
LucasBarbosaSilva <blucas.oficial@gmail.com>
2020-12-17 18:51:26 -0300

    modificando o Title

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
-// In App.js in a new project
-
 import * as React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text } from 'react-native';
+import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import AppStack from './routes/AppStack';
+import { Poppins_300Ligth, Poppins_900Black , Poppins_500Medium, useFonts } from '@expo-google-fonts/poppins';
+
 
 export default function App() {
-  return (
-    <>
+  let [fontsLoaded] = useFonts({
+    Poppins_300Ligth,
+    Poppins_900Black,
+    Poppins_500Medium
+  });
+
+  if(!fontsLoaded){
+    return <AppLoading/>
+  }else{
+    return (
       <AppStack/>
-      <StatusBar style="light" />
-    </>
-    
-  );
+    );
+  }
+  
 }
--- routes/AppStack.js
+++ routes/AppStack.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import HomeScreen from '../src/pages/HomeScreen';
+
 const { Navigator, Screen} = createStackNavigator();
 
 function AppStack(){
